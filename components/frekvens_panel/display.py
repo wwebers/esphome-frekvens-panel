9394ab174971914b63d4071ff523bf42a0700b96
Andre Kemper <andre.kemper@innoq.com>
2023-11-28 20:26:22 +0100

    format and use new API

--- components/frekvens_panel/display.py
+++ components/frekvens_panel/display.py
@@ -19,7 +19,6 @@ Panel = frekvenspanel_ns.class_(
     "Panel", cg.PollingComponent, display.DisplayBuffer
 )
 
-
 CONFIG_SCHEMA = cv.All(
     display.FULL_DISPLAY_SCHEMA.extend(
         {
@@ -33,7 +32,6 @@ CONFIG_SCHEMA = cv.All(
     .extend(cv.polling_component_schema("1s")),
     cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -49,6 +47,6 @@ async def to_code(config):
 
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
-            config[CONF_LAMBDA], [(display.DisplayBufferRef, "it")], return_type=cg.void
+            config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
         )
         cg.add(var.set_writer(lambda_))
